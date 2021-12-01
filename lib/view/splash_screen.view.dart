5e5611a889a7e6ae56f041de6f7fd575b3bb9db3
Mind Game <md.izajul@gmail.com>
2021-12-01 07:50:48 +0600

    splash_screen.view.dart added

---
+++ lib/view/splash_screen.view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class SplashScreen extends StatelessWidget {
+  static const routeName = "/";
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
